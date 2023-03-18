57312ff9d8f6b7eacb7f4a7952b8469c67c8dd49
PyBeginner1 <shashvathn@gmail.com>
2023-03-18 13:20:39 +0530

    Flask created

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,11 @@ from src.exception import CustomException
 from src.logger import logging
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import dill
+import warnings
+warnings.filterwarnings('ignore')
 
 def save_obj(file_path, obj):
     try:
@@ -16,11 +19,19 @@ def save_obj(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         logging.info('Model Evaluation has begun')
         report = {}
         for name, model in models.items():
+            model_params = param[name]
+
+            grid = GridSearchCV(model, model_params, cv=3)
+            grid.fit(X_train, y_train)
+            model.set_params(**grid.best_params_)
+            
+            logging.info(f'Training completed for {name}')
+
             model.fit(X_train, y_train)
             prediction = model.predict(X_test)
             score = r2_score(y_test, prediction)
@@ -31,3 +42,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     except Exception as e:
         logging.info(e)
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
